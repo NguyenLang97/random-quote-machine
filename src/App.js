a30bec79b60c5b6fb8a906f5d94915fe3d1bfa78
NguyenLang97 <nguyenvanlang1997@gmail.com>
2022-07-19 16:44:39 +0700

    langcm

--- src/App.js
+++ src/App.js
@@ -16,30 +16,31 @@ function App() {
     const [quotesArray, setQuotesArray] = useState(null);
     const [accentColor, setAccentColor] = useState('#282c34');
     // const [randomNumber, setRandomNumber] = useState(0);
+    const [show, setShow] = useState(false);
 
-    // const fetchQuotes = async (url) => {
-    //     const response = await fetch(url);
-    //     const parsedJSON = await response.json();
-    //     setQuotesArray(parsedJSON.quotes);
-    // };
-
-    const httpRequest = (url) => {
-        var xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = handleResult;
-        xhr.open('GET', url, true);
-        xhr.send();
-
-        function handleResult() {
-            if (xhr.readyState === XMLHttpRequest.DONE) {
-                setQuotesArray(JSON.parse(xhr.responseText).quotes);
-            }
-        }
+    const fetchQuotes = async (url) => {
+        const response = await fetch(url);
+        const parsedJSON = await response.json();
+        setQuotesArray(parsedJSON.quotes);
     };
 
+    // const httpRequest = (url) => {
+    //     var xhr = new XMLHttpRequest();
+    //     xhr.onreadystatechange = handleResult;
+    //     xhr.open('GET', url, true);
+    //     xhr.send();
+
+    //     function handleResult() {
+    //         if (xhr.readyState === XMLHttpRequest.DONE) {
+    //             setQuotesArray(JSON.parse(xhr.responseText).quotes);
+    //         }
+    //     }
+    // };
+
     useEffect(() => {
-        // fetchQuotes(quoteDBUrl);
-        httpRequest(quoteDBUrl);
-    }, [quoteDBUrl]);
+        fetchQuotes(quoteDBUrl);
+        // httpRequest(quoteDBUrl);
+    }, [quote]);
 
     const getRandomQuote = () => {
         let randomInteger = Math.floor(quotesArray.length * Math.random());
@@ -47,16 +48,19 @@ function App() {
         setAccentColor(COLORS_ARRAY[randomInteger]);
         setQuote(quotesArray[randomInteger].quote);
         setAuthor(quotesArray[randomInteger].author);
+        setShow(!show);
     };
 
     return (
         <div className="App">
             <header className="App-header" style={{ backgroundColor: accentColor }}>
                 <div id="quote-box" style={{ color: accentColor }}>
-                    <h2 id="text">
+                    <h2 id="text " className={show ? 'show' : 'hide'}>
                         <span id="quote-icon"></span>"{quote}"
                     </h2>
-                    <p id="author">- {author}</p>
+                    <p className={show ? 'show' : 'hide'} id="author">
+                        - {author}
+                    </p>
                     <div className="buttons">
                         <a
                             id="tweet-quote"
@@ -76,9 +80,11 @@ function App() {
                         <button
                             id="new-quote"
                             style={{ backgroundColor: accentColor }}
-                            onClick={() => getRandomQuote()}
+                            onClick={() => {
+                                getRandomQuote();
+                            }}
                         >
-                            Generate A Random Quote
+                            New Quote
                         </button>
                         {/* <FontAwesomeIcon icon={faTwitter} /> */}
                     </div>
